6bfcdb6c469b9b6c9762b99689d85e70594f2581
Lokesh <jirathorelokesh@gmail.com>
2026-04-06 14:05:37 +0530

    final fix

---
+++ Js/Js first.js
@@ -0,0 +1,12 @@
+//This code prints hello world
+console.log("hello world");
+
+//Artimetic operator
+let a= 5;
+let b= 6;
+
+console.log(" a + b =", a+b);
+console.log(" a - b =", a-b);
+console.log(" a * b =", a*b);
+console.log(" a / b =", a/b);
+console.log(" a ** b =", a**b);
