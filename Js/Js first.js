5bfafc1ea91895b66466723fc4acf3c42494ede9
Lokesh <jirathorelokesh@gmail.com>
2026-04-15 12:33:21 +0530

    mobile friendly changes

--- Js/Js first.js
+++ Js/Js first.js
@@ -2,11 +2,18 @@
 console.log("hello world");
 
 //Artimetic operator
-let a= 5;
+/*let a= 5;
 let b= 6;
 
 console.log(" a + b =", a+b);
 console.log(" a - b =", a-b);
 console.log(" a * b =", a*b);
 console.log(" a / b =", a/b);
-console.log(" a ** b =", a**b);
+console.log(" a ** b =", a**b);*/
+
+let a = 5;
+let b = 2;
+
+a += 4; //a = a+4
+console.log("a = ", a); //9
+
